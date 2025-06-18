e473c057aa2e8833335918f08646fec36269b65d
wzjsun <530739162@qq.com>
2025-06-18 18:20:25 +0800

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -54,13 +54,13 @@ impl<T, const N0: usize, const N: usize, const L: usize> Timer<T, N0, N, L> {
     }
 
      /// 在当前时间之后，放入一个定时任务
-    pub fn push_time(&mut self, time: u64, el: T) -> TimerKey {
+    pub fn push_time(&mut self, time: u64, el: T) {
         self.push(match time.checked_sub(self.roll_count) {
             Some(r) => r as usize,
             _ => 0,
         }, el)
     }
-    
+
     /// 放入一个定时任务
     pub fn push(&mut self, timeout: usize, el: T) {
         self.add_count += 1;
